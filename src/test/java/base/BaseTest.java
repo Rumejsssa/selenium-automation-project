fdc3fcc285c29b7e54152df52ddd4665e011de95
Rumejsa <xhelilixhelilirumejsa@gmail.com>
2026-03-14 15:44:39 +0100

    Add test classes and project structure

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,12 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
 
 import java.time.Duration;
 
 public class BaseTest {
 
-    WebDriver driver;
+    protected WebDriver driver;
 
     @BeforeMethod
     public void setup() {
